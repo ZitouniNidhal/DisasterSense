0106e9f2c0c7ddb5f009011bd3ec4fe86b2dbb71
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2025-01-07 21:23:06 +0100

    Add evaluate method to MLP class for model performance assessment

--- src/mlp.h
+++ src/mlp.h
@@ -10,6 +10,7 @@ public:
     MLP(const std::vector<int>& layer_sizes);
     void train(const std::vector<std::vector<double>>& X,
                const std::vector<int>& y, int epochs, double learning_rate);
+      double evaluate(const std::vector<std::vector<double>>& test_data, const std::vector<int>& test_labels);          
     std::vector<double> forward(const std::vector<double>& inputs);
     void backpropagate(const std::vector<std::vector<double>>& X,
                        const std::vector<int>& y, double learning_rate);
