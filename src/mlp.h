ae2507c22827bd129bfe0ec8cca222a94fea1f06
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2025-01-10 23:22:49 +0100

    Refactor MLP class to use smart pointers for weights and biases, and add input validation methods

--- src/mlp.h
+++ src/mlp.h
@@ -4,25 +4,29 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <memory>
 
 class MLP {
-public:
-    MLP(const std::vector<int>& layer_sizes);
+ MLP(const std::vector<int>& layer_sizes);
     void train(const std::vector<std::vector<double>>& X,
-               const std::vector<int>& y, int epochs, double learning_rate);
-      double evaluate(const std::vector<std::vector<double>>& test_data, const std::vector<int>& test_labels);          
-    std::vector<double> forward(const std::vector<double>& inputs);
+               const std::vector<int>& y, const int epochs, const double learning_rate);
+    double evaluate(const std::vector<std::vector<double>>& test_data, const std::vector<int>& test_labels) const;          
+    std::vector<double> forward(const std::vector<double>& inputs) const;
     void backpropagate(const std::vector<std::vector<double>>& X,
-                       const std::vector<int>& y, double learning_rate);
-    void update_weights(double learning_rate);
+                       const std::vector<int>& y, const double learning_rate);
+    void update_weights(const double learning_rate);
 
 private:
-    std::vector<std::vector<std::vector<double>>> weights;  // Poids des couches
-    std::vector<std::vector<double>> biases;               // Biais des couches
-    std::vector<std::vector<double>> activations;         // Activations des couches
-    std::vector<std::vector<double>> z_values;            // Valeurs de la fonction z (entrées avant activation)
-    std::vector<std::vector<double>> delta;               // Delta pour la rétropropagation
-    std::vector<int> layer_sizes;                          // Taille de chaque couche
+    void initialize_weights_and_biases();
+    void check_input_size(const std::vector<double>& inputs) const;
+    void check_training_data(const std::vector<std::vector<double>>& X, const std::vector<int>& y) const;
+
+    std::vector<std::unique_ptr<std::vector<std::vector<double>>>> weights;  // Poids des couches
+    std::vector<std::unique_ptr<std::vector<double>>> biases;               // Biais des couches
+    std::vector<std::unique_ptr<std::vector<double>>> activations;         // Activations des couches
+    std::vector<std::unique_ptr<std::vector<double>>> z_values;            // Valeurs de la fonction z (entrées avant activation)
+    std::vector<std::unique_ptr<std::vector<double>>> delta;               // Delta pour la rétropropagation
+    std::vector<int> layer_sizes;                                          // Taille de chaque couche
 };
 
 #endif
